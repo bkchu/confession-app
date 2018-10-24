8e8dde0081894d2bf0daa782ffdc85a682f1e729
Brandon Chung <brandon.k.chung@gmail.com>
2018-10-24 00:50:31 -0500

    Updates

---
+++ precache-manifest.3629fdc372a01c1e8b3cb65703fc0da6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e93059e3d4ea0743ffc29dde5eb8566",
+    "url": "/confession-app/static/media/burger-menu.4e93059e.svg"
+  },
+  {
+    "revision": "222ace56bfbfb891bfa45a42a82f1fcf",
+    "url": "/confession-app/static/media/burger-menu-collapse.222ace56.svg"
+  },
+  {
+    "revision": "55139806354c9cf47f27",
+    "url": "/confession-app/static/js/runtime~main.55139806.js"
+  },
+  {
+    "revision": "1ee665f26ffe12960803",
+    "url": "/confession-app/static/js/main.1ee665f2.chunk.js"
+  },
+  {
+    "revision": "b035f7a3afa716972735",
+    "url": "/confession-app/static/js/1.b035f7a3.chunk.js"
+  },
+  {
+    "revision": "1ee665f26ffe12960803",
+    "url": "/confession-app/static/css/main.16bc5f8d.chunk.css"
+  },
+  {
+    "revision": "b035f7a3afa716972735",
+    "url": "/confession-app/static/css/1.76a09a06.chunk.css"
+  },
+  {
+    "revision": "a9027b3301cf6d11c2f4484f5b1134ed",
+    "url": "/confession-app/index.html"
+  }
+];
